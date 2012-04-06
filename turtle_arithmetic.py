657786c704fd48fe011a2c51938af10a78e210f6
Zack M. Davis <code@zackmdavis.net>
2012-04-05 22:59:03 -0700

    times sign; and, more refactoring and input validation

--- turtle_arithmetic.py
+++ turtle_arithmetic.py
@@ -176,6 +176,21 @@ class CalculatorTurtle(turtle.RawTurtle):
         self.forward(0.8*self.width)
         self.penup()        
 
+    def times(self, x, y):
+        self.penup()
+        waypoint = self.make_block_waypoint(0.1, 0.74, x, y)
+        self.to_waypoint(waypoint)
+        self.pendown()
+        waypoint = self.make_block_waypoint(0.9, 0.26, x, y)
+        self.to_waypoint(waypoint)
+        self.penup()
+        waypoint = self.make_block_waypoint(0.9, 0.74, x, y)
+        self.to_waypoint(waypoint)
+        self.pendown()
+        waypoint = self.make_block_waypoint(0.1, 0.26, x, y)
+        self.to_waypoint(waypoint)
+        self.penup()
+
     def bottom_line(self, x, y, length):
         self.penup()
         waypoint = self.make_block_waypoint(0, 0, x, y)
@@ -194,33 +209,41 @@ class CalculatorTurtle(turtle.RawTurtle):
         self.to_waypoint(waypoint)
         self.penup()
 
-    def add(self, summand1, summand2, x, y):
-        summands_length = max([len(s) for s in [summand1, summand2]])
-        result_length = summands_length + 1
-        summands = [s.zfill(summands_length) for s in [summand1, summand2]]
-        for i, s in enumerate(summands):
+    def statement(self, arg1, arg2, op, x, y):
+        args_length = max([len(a) for a in [arg1, arg2]])
+        args = [s.zfill(args_length) for s in [arg1, arg2]]
+        for i, s in enumerate(args):
             leading_zeros = True
             for j, figure in enumerate(s):
                 if not leading_zeros or figure!='0':
                     leading_zeros = False
                     draw_digit = self.symbols[figure]
                     draw_digit(x+j+1, y-i)
-            if i == len(summands)-2:
-                self.plus(x, y-1)
-            if i == len(summands)-1:
-                self.bottom_line(x, y-1, summands_length+1)
+            if i == len(args)-2:
+                if op == '+':
+                    self.plus(x, y-1)
+                elif op == '-':
+                    self.minus(x, y-1)
+                elif op == 'x':
+                    self.times(x, y-1)
+            if i == len(args)-1:
+                self.bottom_line(x, y-1, args_length+1)
+        return args
+
+    def add(self, summand1, summand2, x, y):
+        summands = ['0'+s for s in self.statement(summand1, summand2, '+', x, y)]
+        summands_length = len(summands[0])
         carry = 0
-        summands = ['0'+s for s in summands]
-        for i in range(1, result_length+1):
+        for i in range(1, summands_length+1):
             place_sum = sum([int(s[-i]) for s in summands])
             place_sum += carry
             place_sum = str(place_sum).zfill(2)
-            if not (i==result_length and place_sum[-1]=='0'):
+            if not (i==summands_length and place_sum[-1]=='0'):
                 draw_result_digit = self.symbols[place_sum[-1]]
-                draw_result_digit(x+summands_length+1-i, y-2)
+                draw_result_digit(x+summands_length-i, y-2)
             if place_sum[-2]!='0':
                 draw_carry_digit = self.symbols[place_sum[-2]]
-                draw_carry_digit(x+summands_length-i, y+1)
+                draw_carry_digit(x+summands_length-1-i, y+1)
             carry = int(place_sum[-2])
         self.forward(45)
 
@@ -241,6 +264,7 @@ class TurtleArithmetic(tkinter.Tk):
 
         self.menu_bar = tkinter.Menu(self)
         self.file_menu = tkinter.Menu(self.menu_bar, tearoff=0)
+        # BUG: can't quit from menu while turtle is drawing
         self.file_menu.add_command(label="Quit", command=self.quit)
         self.menu_bar.add_cascade(label="File", menu=self.file_menu)
 
@@ -282,7 +306,7 @@ class TurtleArithmetic(tkinter.Tk):
         self.add_button = tkinter.Button(self, text="Subtract", command=lambda: self.operation('-'))
         self.add_button.grid(row=3, column=1)
 
-        self.add_button = tkinter.Button(self, text="Multiply", command=lambda: self.operation('+'))
+        self.add_button = tkinter.Button(self, text="Multiply", command=lambda: self.operation('x'))
         self.add_button.grid(row=3, column=2)
 
         self.add_button = tkinter.Button(self, text="Divide", command=lambda: self.operation('/'))
@@ -318,30 +342,41 @@ class TurtleArithmetic(tkinter.Tk):
         #  way, the program can support larger numbers (and long
         #  division) without making all calculations unduly small
 
-    # IDEA: also, instead of doing only a single binary operation from
-    # two text fields, parse an abitrary arithmetic expression and do
-    # all the calculations indicated
-
     def operation(self, op):
-        # TODO: make error message explicitly name which input is bad
-        # TODO: also, check for spaces---Python's int() handles them
+        # TODO: check for spaces---Python's int() handles them
         # intelligently, but my 'add' method does not
         a, b = self.first_number_field.get(), self.second_number_field.get()
+        nonnumbers = []
         try:
-            m = int(a); n = int(b)
+            m = int(a)
         except ValueError:
-            tkinter.messagebox.showinfo("Error", "The turtle doesn't understand.")
+            nonnumbers.append(a)
+        try:
+            n = int(b)
+        except ValueError:
+            nonnumbers.append(b)
+        if nonnumbers:
+            if len(nonnumbers) == 1:
+                error_tail = '"' + nonnumbers[0] + '" is not a number.'
+            elif len(nonnumbers) == 2:
+                error_tail = '"' + nonnumbers[0] + '" and "' + nonnumbers[1] + '" are not numbers.'
+            tkinter.messagebox.showerror("Turtle Comprehension Error", "The turtle doesn't understand: " + error_tail)
             return
         for i in range(2):
             self.appearance_menu.entryconfig(i, state=tkinter.DISABLED)
         self.our_heroine.clear()
-        if op=='+':
+        if op == '+':
             self.our_heroine.add(a, b, 4, 4)
-        elif op=='-':
+        elif op == '-':
+            if m < n:
+                tkinter.messagebox.showerror("Turtle Ignorant of Negative Numbers", "The turtle doesn't know how to subtract a bigger number from a smaller one.")
+                for i in range(2):
+                    self.appearance_menu.entryconfig(i, state=tkinter.NORMAL)
+                return
             pass # TODO
-        elif op=='x':
+        elif op == 'x':
             pass # TODO
-        elif op=='/':
+        elif op == '/':
             pass # TODO
         for i in range(2):
             self.appearance_menu.entryconfig(i, state=tkinter.NORMAL)
